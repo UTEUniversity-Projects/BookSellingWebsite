f5df146a1c05efa88d205d25e3022aae87b0c193
NIX <121375847+loikimtrang@users.noreply.github.com>
2024-10-15 16:03:49 +0700

    add: entity - 8386 (#14)

--- src/main/java/com/biblio/enumeration/EPromotionStatus.java
+++ src/main/java/com/biblio/enumeration/EPromotionStatus.java
@@ -1,4 +1,26 @@
 package com.biblio.enumeration;
 
 public enum EPromotionStatus {
+    NULL_AND_VOID("Promotion is null and void"),
+    EFFECTIVE("Promotion is currently effective"),
+    USED_OUT("Promotion has been used out"),
+    EXPIRED("Promotion has expired");
+
+    private String description;
+
+    // Constructor
+    EPromotionStatus(String description) {
+        this.description = description;
+    }
+
+    // Getter for description
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return name() + " (" + description + ")";
+    }
 }
+
--- src/main/java/com/biblio/enumeration/EPromotionTargetType.java
+++ src/main/java/com/biblio/enumeration/EPromotionTargetType.java
@@ -1,4 +1,28 @@
 package com.biblio.enumeration;
 
 public enum EPromotionTargetType {
+    public enum EPromotionType {
+        DISCOUNT("Percentage or fixed amount discount"),
+        COUPON("A coupon with a specific code"),
+        VOUCHER("A voucher applicable to orders"),
+        FREESHIP("Free shipping promotion");
+
+        private String description;
+
+        // Constructor
+        EPromotionType(String description) {
+            this.description = description;
+        }
+
+        // Getter for description
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String toString() {
+            return name() + " (" + description + ")";
+        }
+    }
+
 }
