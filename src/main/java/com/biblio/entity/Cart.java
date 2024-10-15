9cf91c87b3084f8d49eee76cebc26d582b427daf
loikimtrang <tkloi2004st@gmail.com>
2024-10-15 16:02:32 +0700

    add: entity - 8386

--- src/main/java/com/biblio/entity/Cart.java
+++ src/main/java/com/biblio/entity/Cart.java
@@ -3,4 +3,40 @@ package com.biblio.entity;
 import java.io.Serializable;
 
 public class Cart implements Serializable {
+    private String id;
+    private Customer customer;
+    private OrderItem[] books;
+
+    public Cart() {
+    }
+
+    public Cart(String id, Customer customer, OrderItem[] books) {
+        this.id = id;
+        this.customer = customer;
+        this.books = books;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public OrderItem[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(OrderItem[] books) {
+        this.books = books;
+    }
 }
--- src/main/java/com/biblio/entity/MediaFile.java
+++ src/main/java/com/biblio/entity/MediaFile.java
@@ -3,4 +3,40 @@ package com.biblio.entity;
 import java.io.Serializable;
 
 public class MediaFile implements Serializable {
+    private String id;
+    private String fileName;
+    private String storedCode;
+
+    public MediaFile() {
+    }
+
+    public MediaFile(String id, String fileName, String storedCode) {
+        this.id = id;
+        this.fileName = fileName;
+        this.storedCode = storedCode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getStoredCode() {
+        return storedCode;
+    }
+
+    public void setStoredCode(String storedCode) {
+        this.storedCode = storedCode;
+    }
 }
