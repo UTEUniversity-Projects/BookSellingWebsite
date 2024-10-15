0889c5fbc0e704928b710cdb5c62bde394e94a67
NIX <121375847+loikimtrang@users.noreply.github.com>
2024-10-15 12:58:28 +0700

    Entity nixx (#8)
    * Base be (#5) (#6)
    
    * fix unrelated histories (#3)
    
    * first commit
    
    * add: README.md
    
    * add: decorators
    
    * change: com.mebook -> com.biblio
    
    * add: enumeration, constants/Constant
    
    * add: pÃom.xml/javax.mail
    
    * fix
    
    * add javax mail (#4)
    
    * first commit
    
    * add: README.md
    
    * add: decorators
    
    * change: com.mebook -> com.biblio
    
    * add: enumeration, constants/Constant
    
    * add: pÃom.xml/javax.mail
    
    * fix
    
    ---------
    
    
    
    ---------
    
    Co-authored-by: dopamine <88769856+LeTanTru@users.noreply.github.com>
    Co-authored-by: NIX <121375847+loikimtrang@users.noreply.github.com>
    Co-authored-by: LeTanTru <letantru@gmail.com>
    
    * add init entity class - nix
    
    ---------
    
    Co-authored-by: Le Hong Phuc <84840881+PhucLe0809@users.noreply.github.com>
    Co-authored-by: dopamine <88769856+LeTanTru@users.noreply.github.com>
    Co-authored-by: LeTanTru <letantru@gmail.com>

---
+++ src/main/java/com/biblio/entity/ContributorProfile.java
@@ -0,0 +1,6 @@
+package com.biblio.entity;
+
+import java.io.Serializable;
+
+public abstract class ContributorProfile implements Serializable {
+}
---
+++ src/main/java/com/biblio/entity/Publisher.java
@@ -0,0 +1,6 @@
+package com.biblio.entity;
+
+import java.io.Serializable;
+
+public class Publisher extends ContributorProfile implements Serializable {
+}
