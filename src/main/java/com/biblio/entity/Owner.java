7af2eec62bd94d9a3d9ce8f6cb222356af831b8b
huypham <119232977+Huy080204@users.noreply.github.com>
2024-10-15 15:22:47 +0700

    Modified entity by HUY (#12)

--- src/main/java/com/biblio/entity/Owner.java
+++ src/main/java/com/biblio/entity/Owner.java
@@ -1,6 +1,12 @@
 package com.biblio.entity;
 
-import java.io.Serializable;
+import com.biblio.enumeration.EGender;
 
-public class Owner implements Serializable {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Owner extends User implements Serializable {
+    public Owner(String id, String userName, String fullName, String password, String emailAddress, String dateOfBirth, EGender gender, String phoneNumber, MediaFile avatar, Date joinAt) {
+        super(id, userName, fullName, password, emailAddress, dateOfBirth, gender, phoneNumber, avatar, joinAt);
+    }
 }
