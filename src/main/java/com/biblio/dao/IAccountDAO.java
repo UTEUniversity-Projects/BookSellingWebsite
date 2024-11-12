52d3665dfff8b1eafa3906489fac633d0c91e399
ThuyLinh09 <TieuHoLy.09>
2024-11-12 09:32:10 +0700

    update: login

--- src/main/java/com/biblio/dao/IAccountDAO.java
+++ src/main/java/com/biblio/dao/IAccountDAO.java
@@ -1,7 +1,9 @@
 package com.biblio.dao;
 
+import com.biblio.entity.Account;
+
 public interface IAccountDAO {
 
     boolean isUsernameExist(String username);
-
+    Account getAccountByUsername(String username);
 }
