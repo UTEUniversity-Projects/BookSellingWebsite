14ff073965a269b27e23c398d7219bf0085644e6
huypham <119232977+Huy080204@users.noreply.github.com>
2024-11-12 16:11:06 +0700

    add file support-customer-details.jsp, SupportRequest , modify: support-customer-dashboard, ISupportService + impl, ISupportDAO + impl, INotificationDAO + impl, ICustomerDAO + Impl, modify FetchType of customer_notification in Customer = EAGER, modify FetchType of customer_id in Support = EAGER (#116)
    Co-authored-by: vubaolongkg <156653049+vubaolongkg@users.noreply.github.com>

--- src/main/java/com/biblio/dao/ICustomerDAO.java
+++ src/main/java/com/biblio/dao/ICustomerDAO.java
@@ -18,4 +18,6 @@ public interface ICustomerDAO extends IGenericDAO<Customer> {
     boolean isEmailExisted(String email);
 
     boolean isPhoneNumberExisted(String phoneNumber);
+
+    public void updateSupport_Notification(Customer customer);
 }
