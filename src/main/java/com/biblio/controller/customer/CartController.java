b8c8a40eb0eb1653d3723825c39649731d2132eb
huypham <119232977+Huy080204@users.noreply.github.com>
2024-11-22 11:15:38 +0700

    Update add cáºart item (#200)
    Co-authored-by: Nhut Anh <leenanhh19@gmail.com>

--- src/main/java/com/biblio/controller/customer/CartController.java
+++ src/main/java/com/biblio/controller/customer/CartController.java
@@ -1,10 +1,16 @@
 package com.biblio.controller.customer;
 
+import com.biblio.dto.response.AccountGetResponse;
+import com.biblio.dto.response.CartResponse;
+import com.biblio.service.ICartService;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.Serial;
 
@@ -15,6 +21,9 @@ import java.io.Serial;
 public class CartController extends HttpServlet {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Inject
+    private ICartService cartService;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -30,8 +39,12 @@ public class CartController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
 
+        HttpSession session = request.getSession();
+        AccountGetResponse account = (AccountGetResponse) session.getAttribute("account");
 
+        CartResponse cart = cartService.getCartResponseByUsername(account.getUsername());
 
+        request.setAttribute("cart", cart);
         request.setAttribute("breadcumb", "Giỏ hàng");
         request.getRequestDispatcher("/views/customer/cart.jsp").forward(request, response);
     }
