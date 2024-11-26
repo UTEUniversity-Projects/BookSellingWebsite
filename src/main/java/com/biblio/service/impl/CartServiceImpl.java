f4643e60b25a4f2e92d3d46537dc9e0709976cae
dopamine <88769856+LeTanTru@users.noreply.github.com>
2024-11-26 23:24:25 +0800

    Add dÆelete cáºart item (#233)
    Co-authored-by: Nhut Anh <leenanhh19@gmail.com>

--- src/main/java/com/biblio/service/impl/CartServiceImpl.java
+++ src/main/java/com/biblio/service/impl/CartServiceImpl.java
@@ -3,14 +3,11 @@ package com.biblio.service.impl;
 import com.biblio.dao.IBookTemplateDAO;
 import com.biblio.dao.ICartDAO;
 import com.biblio.dao.ICartItemDAO;
-import com.biblio.dao.ICustomerDAO;
 import com.biblio.dto.request.AddToCartRequest;
-import com.biblio.dto.request.UpdateCartItemRequest;
 import com.biblio.dto.response.CartResponse;
 import com.biblio.entity.BookTemplate;
 import com.biblio.entity.Cart;
 import com.biblio.entity.CartItem;
-import com.biblio.entity.Customer;
 import com.biblio.mapper.CartMapper;
 import com.biblio.service.ICartService;
 
@@ -54,19 +51,6 @@ public class CartServiceImpl implements ICartService {
             newItem.setBookTemplate(bookTemplate);
             newItem.setQuantity(request.getQuantity());
             cartItemDAO.addCartItem(newItem);
-        }
-    }
-
-    @Override
-    public void updateCart(UpdateCartItemRequest request) {
-        Cart cart = cartDAO.findByAccountId(request.getAccountId());
-        CartItem existingItem = cart.getCartItems().stream()
-                .filter(item -> item.getBookTemplate().getId().equals(request.getBookTemplateId()))
-                .findFirst()
-                .orElse(null);
-        if (existingItem != null) {
-            existingItem.setQuantity(request.getQuantity());
-            cartItemDAO.updateCartItem(existingItem);
         }
     }
 }
