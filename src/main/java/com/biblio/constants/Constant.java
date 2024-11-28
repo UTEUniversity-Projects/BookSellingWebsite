a01e3f645a9ef53dd691d8e4afd5fbf3ae04b6ee
vubaolongkg <156653049+vubaolongkg@users.noreply.github.com>
2024-11-28 16:27:58 +0700

    upadate OrderAPI

--- src/main/java/com/biblio/constants/Constant.java
+++ src/main/java/com/biblio/constants/Constant.java
@@ -2,11 +2,11 @@ package com.biblio.constants;
 
 public class Constant {
     private static final String HOST_NAME = "localhost";
-    private static final String PORT = "3306";
+    private static final String PORT = "3308";
     private static final String DB_NAME = "bsw-biblio";
     public static final String DB_URL = "jdbc:mysql://" + HOST_NAME + ":" + PORT + "/" + DB_NAME;
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "123456";
+    public static final String PASSWORD = "";
 
     public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String DB_MIN_CONNECTIONS = "5";
