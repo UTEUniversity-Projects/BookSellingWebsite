b2e240e39ef77ded26d09babda7a55df8f7c85f9
dopamine <88769856+LeTanTru@users.noreply.github.com>
2024-12-02 08:56:56 +0700

    some changes (#290)

--- src/main/webapp/assets/customer/js/register.js
+++ src/main/webapp/assets/customer/js/register.js
@@ -11,172 +11,88 @@ $(document).ready(() => {
 
 		async handleRegistration () {
 
-			//const registerButton = $('.btn-register');
-			//const spinner = registerButton.find('.spinner');
-			//const buttonText = registerButton.find('.button-text');
+			const registerButton = $('.btn-register');
+			const spinner = registerButton.find('.spinner');
+			const buttonText = registerButton.find('.button-text');
+
+			registerButton.prop('disabled', true);
+			buttonText.addClass('hidden');
+			spinner.removeClass('hidden');
+
+			//let avatar = `${contextPath}/assets/customer/img/avatar/img.png`;
 			//
-			//registerButton.prop('disabled', true);
-			//buttonText.addClass('hidden');
-			//spinner.removeClass('hidden');
-
-			let avatar = `${contextPath}/assets/customer/img/avatar/img.png`;
-
-			const postData = await readImages('avatar');
-			if (postData.length > 0) {
-				const res = await uploadImageToDrive(postData);
-				console.log(res);
-				avatar = res[0].link;
-			}
+			//const postData = await readImages('avatar');
+			//if (postData.length > 0) {
+			//	const res = await uploadImageToDrive(postData);
+			//	console.log(res);
+			//	avatar = res[0].link;
+			//}
 			//console.log(avatar);
 
-			//let avatar = await uploadImage('customer', '#avatar');
-			//if (!avatar) {
-			//	avatar = `${contextPath}/assets/customer/img/avatar/img.png`;
-			//}
-			//console.log({ avatar });
+			let avatar = await uploadImage('customer', '#avatar');
+			if (!avatar) {
+				avatar = `${contextPath}/assets/customer/img/avatar/img.png`;
+			}
+			console.log({ avatar });
 
-			//const formData = new FormData(document.getElementById('registerForm'));
-			//
-			//const userData = {
-			//	fullName: formData.get('fullName'),
-			//	email: formData.get('email'),
-			//	phoneNumber: formData.get('phoneNumber'),
-			//	dateOfBirth: formData.get('dateOfBirth'),
-			//	gender: formData.get('gender'),
-			//	username: formData.get('username'),
-			//	password: formData.get('password'),
-			//	province: formData.get('province'),
-			//	district: formData.get('district'),
-			//	village: formData.get('village'),
-			//	detail: formData.get('detail'),
-			//	avatar: avatar
-			//};
-			//
-			//$.ajax({
-			//	url: `${contextPath}/api/customer/register`,
-			//	type: 'POST',
-			//	contentType: 'application/json',
-			//	data: JSON.stringify(userData),
-			//	success: function (response, status, xhr) {
-			//		console.log(response);
-			//		if (response.code === 200) {
-			//			toast({
-			//				title: 'Đăng ký',
-			//				message: 'Đăng ký thành công',
-			//				type: 'success',
-			//				duration: 3000
-			//			});
-			//			setTimeout(() => {
-			//				window.location.href = `${contextPath}/login`
-			//			}, 1000);
-			//		} else {
-			//			toast({
-			//				title: 'Đăng ký',
-			//				message: 'Thông tin đã tồn tại !',
-			//				type: 'error',
-			//				duration: 3000
-			//			});
-			//			Object.keys(response).forEach(key => {
-			//				if (key !== 'code') {
-			//					$(`#${key}`).next().text(response[key]);
-			//				}
-			//			});
-			//		}
-			//	},
-			//	error: function (xhr, status, error) {
-			//		console.log(error);
-			//		toast({
-			//			title: 'Lỗi',
-			//			message: 'Có lỗi xảy ra',
-			//			type: 'error',
-			//			duration: 3000
-			//		});
-			//
-			//	} catch (error) {
-			//		console.error('Error uploading file:', error);
-			//		return null;
-			//	}
-			//};
+			const formData = new FormData(document.getElementById('registerForm'));
 
-			//const handleRegistration = async () => {
-			//
-			//	let avatar = await uploadImage('customer', '#avatar');
-			//	if (!avatar) {
-			//		avatar = `${contextPath}/assets/customer/img/avatar/img.png`;
-			//	}
-			//	console.log(avatar);
-			//
-			//	const formData = new FormData(document.getElementById("registerForm"));
-			//
-			//	const userData = {
-			//		fullName: formData.get("fullName"),
-			//		email: formData.get("email"),
-			//		phoneNumber: formData.get("phoneNumber"),
-			//		dateOfBirth: formData.get("dateOfBirth"),
-			//		gender: formData.get("gender"),
-			//		username: formData.get("username"),
-			//		password: formData.get("password"),
-			//		province: formData.get("province"),
-			//		district: formData.get("district"),
-			//		village: formData.get("village"),
-			//		detail: formData.get("detail"),
-			//		avatar: avatar
-			//	};
-			//
-			//	const registerButton = $('.btn-register');
-			//	const spinner = registerButton.find('.spinner');
-			//	const buttonText = registerButton.find('.button-text');
-			//
-			//	registerButton.prop('disabled', true);
-			//	buttonText.addClass('hidden');
-			//	spinner.removeClass('hidden');
-			//
-			//	$.ajax({
-			//		url: `${contextPath}/api/customer/register`,
-			//		type: 'POST',
-			//		contentType: 'application/json',
-			//		data: JSON.stringify(userData),
-			//		success: function (response, status, xhr) {
-			//			console.log(response);
-			//			if (response.code === 200) {
-			//				toast({
-			//					title: "Đăng ký",
-			//					message: "Đăng ký thành công",
-			//					type: "success",
-			//					duration: 3000,
-			//				});
-			//				setTimeout(() => {
-			//					window.location.href = `${contextPath}/login`;
-			//				}, 1000);
-			//			} else {
-			//				toast({
-			//					title: "Đăng ký",
-			//					message: "Thông tin đã tồn tại !",
-			//					type: "error",
-			//					duration: 3000,
-			//				})
-			//				Object.keys(response).forEach(key => {
-			//					if (key !== "code") {
-			//						$(`#${key}`).next().text(response[key]);
-			//					}
-			//				});
-			//			}
-			//		},
-			//		error: function (xhr, status, error) {
-			//			console.log(error);
-			//			toast({
-			//				title: "Lỗi",
-			//				message: "Có lỗi xảy ra",
-			//				type: "error",
-			//				duration: 3000
-			//			})
-			//		},
-			//		complete: function () {
-			//			registerButton.prop('disabled', false);
-			//			buttonText.removeClass('hidden');
-			//			spinner.addClass('hidden');
-			//		}
-			//	});
+			const userData = {
+				fullName: formData.get('fullName'),
+				email: formData.get('email'),
+				phoneNumber: formData.get('phoneNumber'),
+				dateOfBirth: formData.get('dateOfBirth'),
+				gender: formData.get('gender'),
+				username: formData.get('username'),
+				password: formData.get('password'),
+				province: formData.get('province'),
+				district: formData.get('district'),
+				village: formData.get('village'),
+				detail: formData.get('detail'),
+				avatar: avatar
+			};
+
+			$.ajax({
+				url: `${contextPath}/api/customer/register`,
+				type: 'POST',
+				contentType: 'application/json',
+				data: JSON.stringify(userData),
+				success: function (response, status, xhr) {
+					console.log(response);
+					if (response.code === 200) {
+						toast({
+							title: 'Đăng ký',
+							message: 'Đăng ký thành công',
+							type: 'success',
+							duration: 3000
+						});
+						setTimeout(() => {
+							window.location.href = `${contextPath}/login`
+						}, 1000);
+					} else {
+						toast({
+							title: 'Đăng ký',
+							message: 'Thông tin đã tồn tại !',
+							type: 'error',
+							duration: 3000
+						});
+						Object.keys(response).forEach(key => {
+							if (key !== 'code') {
+								$(`#${key}`).next().text(response[key]);
+							}
+						});
+					}
+				},
+				error: function (xhr, status, error) {
+					console.log(error);
+					toast({
+						title: 'Lỗi',
+						message: 'Có lỗi xảy ra',
+						type: 'error',
+						duration: 3000
+					});
+				}
+			})
 
 		}
 
@@ -266,33 +182,33 @@ $(document).ready(() => {
 	const register = new Register();
 	register.getAddress();
 
-	//register.showRequiredInput();
+	register.showRequiredInput();
 
 	Validator({
 		form: '#registerForm',
 		formGroupSelector: '.form-group',
 		errorSelector: '.form-message',
 		rules: [
-			//Validator.isRequired('#fullName'),
-			//Validator.isRequired('#email'),
-			//Validator.isRequired('#phoneNumber'),
-			//Validator.isRequired('#dob', 'Vui lòng chọn ngày sinh !'),
-			//Validator.isRequired("#username"),
-			//Validator.isRequired('#password'),
-			//Validator.isRequired('#re-password'),
-			//Validator.isConfirmed('#re-password', function () {
-			//	return document.querySelector('#registerForm #password').value;
-			//}, 'Mật khẩu nhập lại không chính xác !'),
-			//Validator.isRequiredSelected('#province', 'Vui lòng chọn Tỉnh thành phố !', 'Tỉnh thành phố'),
-			//Validator.isRequiredSelected('#district', 'Vui lòng chọn Quận Huyện !', 'Quận Huyện'),
-			//Validator.isRequiredSelected('#village', 'Vui lòng chọn Phường Xã !', 'Phường Xã'),
-			//Validator.isRequired('#detail'),
-			//Validator.isChecked('#check-with-link', 'Vui lòng đồng ý với điều khoản và chính sách của Biblio !'),
-			//Validator.isEmail('#email', 'Email không đúng định dạng !'),
-			//Validator.phoneNumber("#phoneNumber", 10, 'Số điện thoại phải bao gồm 10 số và chỉ bao gồm số !'),
-			//Validator.isAllLowercase("#username", 'Username chỉ bao gồm chữ thường và số !'),
-			//Validator.minLength('#username', 8),
-			//Validator.minLength('#password', 8),
+			Validator.isRequired('#fullName'),
+			Validator.isRequired('#email'),
+			Validator.isRequired('#phoneNumber'),
+			Validator.isRequired('#dob', 'Vui lòng chọn ngày sinh !'),
+			Validator.isRequired("#username"),
+			Validator.isRequired('#password'),
+			Validator.isRequired('#re-password'),
+			Validator.isConfirmed('#re-password', function () {
+				return document.querySelector('#registerForm #password').value;
+			}, 'Mật khẩu nhập lại không chính xác !'),
+			Validator.isRequiredSelected('#province', 'Vui lòng chọn Tỉnh thành phố !', 'Tỉnh thành phố'),
+			Validator.isRequiredSelected('#district', 'Vui lòng chọn Quận Huyện !', 'Quận Huyện'),
+			Validator.isRequiredSelected('#village', 'Vui lòng chọn Phường Xã !', 'Phường Xã'),
+			Validator.isRequired('#detail'),
+			Validator.isChecked('#check-with-link', 'Vui lòng đồng ý với điều khoản và chính sách của Biblio !'),
+			Validator.isEmail('#email', 'Email không đúng định dạng !'),
+			Validator.phoneNumber("#phoneNumber", 10, 'Số điện thoại phải bao gồm 10 số và chỉ bao gồm số !'),
+			Validator.isAllLowercase("#username", 'Username chỉ bao gồm chữ thường và số !'),
+			Validator.minLength('#username', 8),
+			Validator.minLength('#password', 8),
 		],
 		onSubmit: async function (data) {
 			await register.handleRegistration();
